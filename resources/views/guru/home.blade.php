b87f151361fb498fa3788a684743ca2e29651320
AhmaDXCV <spamlocated@gmail.com>
2020-11-27 14:29:48 +0700

    modified:   resources/views/guru/home.blade.php 	modified:   resources/views/siswa/home.blade.php

--- resources/views/guru/home.blade.php
+++ resources/views/guru/home.blade.php
@@ -1,5 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Halaman Home Guru</h1>
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Manajemen User</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Buka</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Manajemen Produk</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Buka</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Transaksi</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Buka</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row mt-3">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Transaksi Terbaru</h5>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <td>#</td>
+                            <td>Tanggal</td>
+                            <td>Nama</td>
+                            <td>Barang</td>
+                            <td>Jumlah</td>
+                            <td>Total</td>
+                            <td>Status</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
