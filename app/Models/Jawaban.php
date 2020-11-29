54d3ffdc445f4ae9f354fd000b00e633e3a94781
AhmaDXCV <spamlocated@gmail.com>
2020-11-29 18:54:26 +0700

    new file:   app/Models/Jawaban.php 	new file:   app/Models/Profil.php 	new file:   database/migrations/2020_11_29_114442_create_profils_table.php 	new file:   database/migrations/2020_11_29_114747_create_jawabans_table.php

---
+++ app/Models/Jawaban.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jawaban extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'soal_id',
+        'jawaban_soal',
+        'hasil_periksa',
+        'status'
+    ];
+}
---
+++ database/migrations/2020_11_29_114747_create_jawabans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJawabansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jawabans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('soal_id');
+            $table->string('jawaban_soal');
+            $table->string('hasil_periksa');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jawabans');
+    }
+}
